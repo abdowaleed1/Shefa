260132564015e9add30833df483fff1eeedab209
Omar Gomaa <mrjmh934@gmail.com>
2025-11-11 23:44:31 +0200

    add enums that will use and interface for soft delete

---
+++ Models/Enums/AppointmentStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Models.Enums
+{
+    public enum AppointmentStatus
+    {
+        Pending,
+        Confirmed,
+        Completed,
+        Cancelled,
+        NoShow
+    }
+
+}
---
+++ Models/Enums/PaymentStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Models.Enums
+{
+    public enum PaymentStatus
+    {
+        Pending,
+        Paid,
+        Failed,
+        Refunded
+    }
+
+}
