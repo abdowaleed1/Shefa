64ec3474b13e7e8ffdc63834665020519c0ce8b0
Omar Gomaa <mrjmh934@gmail.com>
2025-11-12 00:46:59 +0200

    add the dbset

--- Models/InterFaces/ISoftDelete.cs
+++ Models/InterFaces/ISoftDelete.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Models.InterFaces
 {
-    internal class ISoftDelete
+    public interface ISoftDelete
     {
+        public bool IsDeleted { get; set; }
     }
 }
