95ed0506caca3e7fbde5c1955a9aa20edf701e3a
Omar Gomaa <mrjmh934@gmail.com>
2025-11-15 14:31:51 +0200

    fix any problem and make init migration

--- Models/Configurations/DBTypes.cs
+++ Models/Configurations/DBTypes.cs
@@ -11,8 +11,9 @@ namespace Models.Configurations
         public const string Int = "int";
         public const string Bit = "bit";
         public const string Date = "date";
-        public const string Time = "time";
+        public const string DateTime = "datetime";
         public const string DateTime2 = "datetime2";
+        public const string date = "date";
         public const string Float = "float";
         public const string Money = "money";
         public const string Decimal18_2 = "decimal(18,2)";
